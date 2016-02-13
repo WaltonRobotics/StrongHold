cd9ccaa64e0a00ffdbbe60834a4ed415f5237039
Mauna Kareti <mauna.kareti@waltonrobotics.org>
2016-02-13 13:22:20 -0500

    this commit will fix

--- AsinkProject/src/org/usfirst/frc/team2974/diagnostics/DiagnosticDataTypeList.java
+++ AsinkProject/src/org/usfirst/frc/team2974/diagnostics/DiagnosticDataTypeList.java
@@ -1,5 +1,10 @@
 package org.usfirst.frc.team2974.diagnostics;
 
 public enum DiagnosticDataTypeList {
+<<<<<<< HEAD
 	VELOCITY_INFO
+=======
+	VelocityInformation,
+	SpeedInformation
+>>>>>>> this commit will fix
 }
