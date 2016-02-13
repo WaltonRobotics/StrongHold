d3b1e6de2905b6fa3813560ad37d8e1d703609a6
John Christian Hults <john.hults@waltonrobotics.org>
2016-02-13 13:15:23 -0500

    Created full framework for mauna to work with WORK THIS TIME

--- AsinkProject/src/org/usfirst/frc/team2974/diagnostics/dataGatherers/VelosityGatherer.java
+++ AsinkProject/src/org/usfirst/frc/team2974/diagnostics/dataGatherers/VelosityGatherer.java
@@ -1,14 +1,22 @@
 package org.usfirst.frc.team2974.diagnostics.dataGatherers;
 
+import java.util.Stack;
+
 import org.usfirst.frc.team2974.diagnostics.DiagnosticDataTypeList;
+import org.usfirst.frc.team2974.diagnostics.messageObjects.VelocityData;
 
 public class VelosityGatherer {
-	public void processDataElement(DiagnosticDataTypeList type, Object object) {
-
+	Stack<VelocityData> velocityList = new Stack<VelocityData>();
+	public void processDataElement(DiagnosticDataTypeList type, VelocityData object) {
+		
+		
+		
+		velocityList.push(object);
+		updateDashboard(1,1);
 	}
 
-	public void updateDashboard() {
-
+	public void updateDashboard(double leftWheelDifference, double rightWheelDifference) {
+		
 	}
 
 	public boolean IsDataElementHandle(DiagnosticDataTypeList type) {
