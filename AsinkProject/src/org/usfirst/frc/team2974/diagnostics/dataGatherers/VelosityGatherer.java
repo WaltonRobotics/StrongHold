57b8082c3cd20f64456be5737c813dc5d6869554
Mauna Kareti <mauna.kareti@waltonrobotics.org>
2016-02-17 18:59:51 -0500

    no message

--- AsinkProject/src/org/usfirst/frc/team2974/diagnostics/dataGatherers/VelosityGatherer.java
+++ AsinkProject/src/org/usfirst/frc/team2974/diagnostics/dataGatherers/VelosityGatherer.java
@@ -5,18 +5,24 @@ import java.util.Stack;
 import org.usfirst.frc.team2974.diagnostics.DiagnosticDataTypeList;
 import org.usfirst.frc.team2974.diagnostics.messageObjects.VelocityData;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class VelosityGatherer {
 	Stack<VelocityData> velocityList = new Stack<VelocityData>();
 	public void processDataElement(DiagnosticDataTypeList type, VelocityData object) {
+		double leftPercentDifference = (object.getPredictedLeftVelocity() - object.getTrueLeftVelocity())/object.getPredictedLeftVelocity();
+		double rightPercentDifference = (object.getPredictedRightVelocity() - object.getTrueRightVelocity())/object.getPredictedRightVelocity();
 		
 		
 		
 		velocityList.push(object);
-		updateDashboard(1,1);
+		updateDashboard(leftPercentDifference ,rightPercentDifference);
 	}
 
-	public void updateDashboard(double leftWheelDifference, double rightWheelDifference) {
-		
+			
+	public void updateDashboard(double leftPercentError, double rightPercentError) {
+		SmartDashboard.putNumber("leftPercentError", leftPercentError);
+		SmartDashboard.putNumber("rightPercentError", rightPercentError);
 	}
 
 	public boolean IsDataElementHandle(DiagnosticDataTypeList type) {
