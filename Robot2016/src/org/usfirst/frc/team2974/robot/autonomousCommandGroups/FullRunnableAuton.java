bbde80c70d33eaafa67314dc5ab2318cf4fd0ae0
NamelessSuperCoder <Marius.juston@hotmail.fr>
2016-09-11 20:20:16 -0400

    added Lowbar
    add an if statement that puts in new LowBar() instead of autonomous command if position is A.

--- Robot2016/src/org/usfirst/frc/team2974/robot/autonomousCommandGroups/FullRunnableAuton.java
+++ Robot2016/src/org/usfirst/frc/team2974/robot/autonomousCommandGroups/FullRunnableAuton.java
@@ -7,11 +7,14 @@ import org.usfirst.frc.team2974.robot.autonomousCommands.TurnToAngle;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class FullRunnableAuton extends CommandGroup{
-	public FullRunnableAuton(CommandGroup obstacleCommand, AutonLocator location){
-		if(obstacleCommand.getName().equals("RockWall")){
+public class FullRunnableAuton extends CommandGroup {
+	public FullRunnableAuton(CommandGroup obstacleCommand, AutonLocator location) {
+		if (obstacleCommand.getName().equals("RockWall")) 
 			addSequential(new TurnToAngle(180));
-		}
+		
+		if(location.getAutonPossibleLocation().equals(AutonPossibleLocation.A))
+			addSequential(new LowBar());
+			
 		addSequential(obstacleCommand);
 		addSequential(new MoveToShoot(location));
 		addSequential(new Shoot());
