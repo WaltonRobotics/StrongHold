bcf1abc7d4e0189f37668f6fa235ed1b43ea507b
NamelessSuperCoder <Marius.juston@hotmail.fr>
2016-09-18 18:57:50 -0400

    cleaned up Message and FileHelper code and added additional "safety features"

--- Robot2016/src/org/usfirst/frc/team2974/dataLogs/FileHelper.java
+++ Robot2016/src/org/usfirst/frc/team2974/dataLogs/FileHelper.java
@@ -4,46 +4,80 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class FileHelper {
-    public static File create(String path) {
-        File temp = null;
+	public static File create(String path) {
+		File temp = null;
 
-        if (path.contains(".")) {
-            temp = new File(path.substring(0, path.lastIndexOf("\\")));
+		if (path.contains(".")) {
+			temp = new File(path.substring(0, path.lastIndexOf("\\")));
 
-            if (temp.mkdirs())
-                System.out.println("Managed to create directory ".concat(temp.getAbsolutePath()));
-            else
-                System.out.println("Did not managed to create directory ".concat(temp.getAbsolutePath()));
+			if (temp.mkdirs())
+				System.out.println("Managed to create directory ".concat(temp.getAbsolutePath()));
+			else
+				System.out.println("Did not managed to create directory ".concat(temp.getAbsolutePath()));
 
-            temp = new File(path);
+			temp = new File(path);
 
-            try {
-                if (temp.createNewFile())
-                    System.out.println("Managed to create ".concat(temp.getName()));
-            } catch (IOException e) {
-                System.out.println("Could not manage to create " + temp.getName());
-            }
-        }
+			try {
+				if (temp.createNewFile())
+					System.out.println("Managed to create ".concat(temp.getName()));
+			} catch (IOException e) {
+				System.out.println("Could not manage to create " + temp.getName());
+			}
+		}
 
-        return temp;
-    }
+		return temp;
+	}
 
-    public static void writeToFile(String message, File file) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
-            writer.append(message);
-            writer.newLine();
-        } catch (IOException e) {
-            System.out.println("Could not write to file");
-        }
-    }
+	public static void writeToFile(String message, File file) {
+		if (file != null && !message.trim().isEmpty())
+			try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+				writer.append(message);
+				writer.newLine();
+			} catch (IOException e) {
+				System.out.println("Could not write to file");
+			}
+	}
 
-    public static String formatFilePath(String path) {
-        if (!File.separator.equals("\\"))
-            while (path.contains("\\"))
-                path = path.replace("\\", File.separator);
+	public static String formatFilePath(String path) {
+		if (!File.separator.equals("\\"))
+			while (path.contains("\\"))
+				path = path.replace("\\", File.separator);
 
-        return path;
-    }
+		return path;
+	}
+
+	public static boolean contains(String message, File file) {
+		if (file != null && !message.trim().isEmpty()) {
+			List<String> list = readFromFile(file);
+
+			if (list != null)
+				for (String lines : list)
+					if (lines.contains(message))
+						return true;
+		}
+		return false;
+	}
+
+	public static List<String> readFromFile(File file) {
+		if (file != null) {
+			List<String> textLines = new ArrayList<>();
+
+			try (Scanner scanner = new Scanner(file)) {
+				while (scanner.hasNext())
+					textLines.add(scanner.nextLine());
+			} catch (Exception e) {
+				System.out.println("could not read from file ".concat(file.getName()));
+			}
+
+			return textLines;
+		}
+
+		System.out.println("There was no file inputed into the readFromFile methjod inside the FileHelper class");
+		return null;
+	}
 }
--- Robot2016/src/org/usfirst/frc/team2974/dataLogs/Message.java
+++ Robot2016/src/org/usfirst/frc/team2974/dataLogs/Message.java
@@ -1,50 +1,54 @@
 package org.usfirst.frc.team2974.dataLogs;
 
+import java.io.File;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Message {
-    public static void addAction(String message) {
-        addAction(message, null);
-    }
+	public static void addAction(String message) {
+		addAction(message, null);
+	}
 
-    public static void addAction(String message, Object object) {
-        addMessage("[ACTION]", message, object, "Action Logs");
-    }
+	public static void addAction(String message, Object object) {
+		addMessage("[ACTION]", message, object, "Action Logs");
+	}
 
-    public static void addError(String message) {
-        addError(message, null);
-    }
+	public static void addError(String message) {
+		addError(message, null);
+	}
 
-    public static void addError(String message, Object object) {
-        addMessage("[ERROR]", message, object, "Error Logs");
-    }
+	public static void addError(String message, Object object) {
+		addMessage("[ERROR]", message, object, "Error Logs");
+	}
 
-    private static void addMessage(String what, String message, Object object, String fileName) {
-        message = String.format("%-9s %19s %s | %s\n", what, getSystemTime("yyyy/MM/dd HH:mm:mm"),
-                object == null ? "" : object.getClass().getName(), message);
+	private static void addMessage(String what, String message, Object object, String fileName) {
+		File file = FileHelper
+				.create(FileHelper.formatFilePath(System.getProperty("user.home").concat("\\Desktop\\Logs\\Log"
+						.concat(getSystemTime("yyyy_MM_dd").concat("\\".concat(fileName).concat(".txt"))))));
 
-        FileHelper.writeToFile(message,
-                FileHelper
-                        .create(FileHelper.formatFilePath(System.getProperty("user.home").concat("\\Desktop\\Logs\\Log"
-                                .concat(getSystemTime("yyyy_MM_dd").concat("\\".concat(fileName).concat(".txt")))))));
+		if (!FileHelper.contains(message, file) && !message.trim().isEmpty()) {
+			message = String.format("%-9s %19s %s | %s\n", what, getSystemTime("yyyy/MM/dd HH:mm:mm"),
+					object == null ? "" : object.getClass().getName(), message);
 
-        System.out.println(message.replaceAll("\n", ""));
-    }
+			FileHelper.writeToFile(message, file);
 
-    public static void addWarning(String message) {
-        addWarning(message, null);
-    }
+			System.out.println(message.replaceAll("\n", ""));
+		}
+	}
 
-    public static void addWarning(String message, Object object) {
-        addMessage("[WARNING]", message, object, "Warning Logs");
-    }
+	public static void addWarning(String message) {
+		addWarning(message, null);
+	}
+
+	public static void addWarning(String message, Object object) {
+		addMessage("[WARNING]", message, object, "Warning Logs");
+	}
 
 	/*
-     * yyyy/MM/dd HH:mm:ss yyyy_MM_dd
+	 * yyyy/MM/dd HH:mm:ss yyyy_MM_dd
 	 */
 
-    private static String getSystemTime(String pattern) {
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(pattern));
-    }
+	private static String getSystemTime(String pattern) {
+		return LocalDateTime.now().format(DateTimeFormatter.ofPattern(pattern));
+	}
 }
